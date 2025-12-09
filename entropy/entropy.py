529882ecfe6a1d530d000583301110bfd5a93acb
ImmanuelCurrah <immanueli.currah@gmail.com>
2025-12-08 23:32:47 -0500

    find start and end coordinates of grid for future entropy calculation

--- entropy/entropy.py
+++ entropy/entropy.py
@@ -1,11 +1,35 @@
+import math
 from cell import Cell
 
 class Entropy:
+    
+    cols: int
 
-    def get_entropy_score(self, grid, index):
-        pass
+    def __init__(self, grid):
+        self.cols = len(grid[0])
+    
+    def get_start_end_coords(self):
+        start_indices = []
+        end_indices = []
 
-    def collapse(self, grid, index):
+        start_coords = []
+        end_coords = []
+
+        for index in range(self.cols):
+            start_index = index * (self.cols - 1)
+            end_index = start_index + (self.cols - 1)
+            start_indices.append(start_index)
+            end_indices.append(end_index)
+
+            start_coords.append((index, 0))
+            end_coords.append((self.cols - 1, index))
+
+        return start_coords, end_coords
+
+
+
+
+    def get_entropy_score(self):
         pass
 
     def is_start(self, grid):
--- grid/grid.py
+++ grid/grid.py
@@ -7,32 +7,39 @@ from entropy import Entropy
 class Grid:
 
     grid = []
+
     cell_size = 0
-    entropy = Entropy()
+
+    entropy: Entropy
+
     random_start = {}
+
     start_tetromino = random.choice(["Upright Tetromino", "Upside Down Tetromino", "Right Lateral Tetromino", "Left Lateral Tetromino"])
 
     def __init__(self, screen_width, screen_height):
         self.cell_size = math.gcd(screen_width, screen_height)
 
-        rows = screen_width // self.cell_size
-        cols = screen_height // self.cell_size
-
-        self.grid = [[Cell() for _ in range(rows)] for _ in range(cols)]
+        self.grid = [[Cell() for _ in range(screen_width // self.cell_size)] for _ in range(screen_height // self.cell_size)]
 
         row_index, col_index = self.__get_random_grid_index()
         self.random_start["row_index"] = row_index
         self.random_start["col_index"] = col_index
+        self.entropy = Entropy(self.grid)
 
     def draw_grid(self, screen):
         is_start = self.entropy.is_start(self.grid)
+        start_coords, end_coords = self.entropy.get_start_end_coords()
 
         for current_row_index, rows in enumerate(self.grid):
             for current_col_index, cell in enumerate(rows):
                 x = current_col_index * self.cell_size
                 y = current_row_index * self.cell_size
 
-                if is_start and current_row_index == self.random_start["row_index"] and current_col_index == self.random_start["col_index"]:
+                if (current_row_index, current_col_index) in set(start_coords):
+                   cell.draw_cell(screen, x, y, self.cell_size, self.start_tetromino)
+                elif (current_col_index, current_row_index) in set(end_coords):
+                    cell.draw_cell(screen, x, y, self.cell_size, self.start_tetromino)
+                elif is_start and current_row_index == self.random_start["row_index"] and current_col_index == self.random_start["col_index"]:
                     cell.draw_cell(screen, x, y, self.cell_size, self.start_tetromino)
                 else:
                     cell.draw_cell(screen, x, y, self.cell_size, "Blank Cell")
